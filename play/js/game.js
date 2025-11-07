f7c215bf5221b8571db369e22bdad3d08d84d6c3
DogeMiner: Community Edition <dogeminerdirectory@gmail.com>
2025-11-06 20:32:05 -0500

    Add to the stars to the name list

--- play/js/game.js
+++ play/js/game.js
@@ -19,7 +19,7 @@ class DogeMinerGame {
             'Timmy', 'Mary', 'Martha', 'Linda', 'Jimothy', 'Scout', 'Barley', 'Cherry', 'Vader', 'Mochatitan',
             'Babylion122', 'rkn', 'Raspy', 'Melon', 'News', 'Rick', 'Sam', 'Josiah', 'Miya', 'Creedoo',
             'Cottage', 'Cheese', 'Bikini', 'Silver', 'Sorrel', 'Kyle', 'DwellingStars', 'Done', 'Peanut', 'Fry',
-            'Bruno', 'Charlie', 'Emmet', 'Lucy', 'Vincent'
+            'Bruno', 'Charlie', 'Emmet', 'Lucy', 'Vincent', 'tothestars693'
         ];
         
         this.helperNames = {
