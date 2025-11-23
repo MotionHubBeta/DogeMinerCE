da9509016d9352a477ae6ba87e6844143b349d5a
MisterTerra <simonhaell@gmail.com>
2025-11-23 17:14:42 +0100

    Add more rules to eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,4 +7,26 @@ export default defineConfig([
   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.browser } },
   { files: ["**/*.js"], languageOptions: { sourceType: "module" } },
   { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
+  {
+    rules: {
+      'no-self-compare': 'error',
+      'no-unassigned-vars': 'error',
+      'no-unmodified-loop-condition': 'error',
+      'no-unreachable-loop': 'error',
+      'no-use-before-define': 'error',
+      'no-useless-assignment': 'error',
+      'require-atomic-updates': 'error',
+      'camelcase': 'error',
+      'class-methods-use-this': 'error',
+      'consistent-this': ['error', 'self'],
+      'default-case': 'error',
+      'default-case-last': 'error',
+      'default-param-last': 'error',
+      'dot-notation': 'error',
+      'eqeqeq': 'error',
+      'guard-for-in': 'error',
+      'func-names': 'error',
+      'consistent-return': 'error'
+    },
+  }
 ]);
